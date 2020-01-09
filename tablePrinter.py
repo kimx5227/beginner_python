ed3b43a731ffc3d36752d61bdb5f4864d1e83c85
kimx5227 <kimx5227@umn.edu>
2020-01-08 21:27:34 -0600

    Added comments and stored array size as tuple

--- tablePrinter.py
+++ tablePrinter.py
@@ -1,16 +1,17 @@
+#! Python 3
+#prints 2-D array with perfect entries as table
+
 def printTable(list):
     colwidth = [0]*len(list)
-    print(colwidth)
-    colSpan = len(list)
-    rowSpan = len(list[0])
+    index = (len(list),len(list[0]))    #column and row position
 
-    for col in range(len(list)):
+    for col in range(len(list)):    #finds maximum colunn width for column
         for index in range(len(list[col])):
             if len(list[col][index]) > colwidth[col]:
                 colwidth[col] = len(list[col][index]) + 2
 
-    for row in range(rowSpan):
-        for col in range(colSpan):
+    for row in range(index[0]): #prints each row for each column
+        for col in range(index[1]):
             print(list[col][row].rjust(colwidth[col]), end = '')
         print('')
 
